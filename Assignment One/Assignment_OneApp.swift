593a3518e8db3fec2863fc1cc28a0b8b350f8e2d
Santiago Gutierrez <ssgutierrez42@gmail.com>
2020-12-25 09:37:26 -0800

    Initial Commit

---
+++ Assignment One/Assignment_OneApp.swift
@@ -0,0 +1,17 @@
+//
+//  Assignment_OneApp.swift
+//  Assignment One
+//
+//  Created by Santiago Gutierrez on 12/25/20.
+//
+
+import SwiftUI
+
+@main
+struct Assignment_OneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
